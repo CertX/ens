96c281d53fe3977ccc36b0e16c2736502bdcb10a
Perfect-SoftwareEngineer <elegantdev199742@gmail.com>
2020-05-22 13:11:15 +0200

    eslint

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -7,13 +7,13 @@ import { NetworkKind } from '@0xcert/ethereum-generic-provider';
  */
 export function getEnsAddress(networkKind: NetworkKind) {
   switch (networkKind) {
-    case NetworkKind.LIVE:
-      return '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
-    case NetworkKind.RINKEBY:
-      return '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
-    case NetworkKind.ROPSTEN:
-      return '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
-    default:
-      throw new Error('Unsupported network kind.');
+  case NetworkKind.LIVE:
+    return '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
+  case NetworkKind.RINKEBY:
+    return '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
+  case NetworkKind.ROPSTEN:
+    return '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
+  default:
+    throw new Error('Unsupported network kind.');
   }
 }
